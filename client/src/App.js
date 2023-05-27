e4cf4c2ad4a85c94b2174079a200a8ae8d6f7ba2
Rounak sharma <sharmarounak014@gmail.com>
2023-05-27 17:53:24 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,40 @@
+import {useEffect} from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    useLocation
+} from 'react-router-dom';
+
+import Home from './scenes/home/Home';
+import Navbar from './scenes/global/Navbar';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+
+
+const ScrollToTop=()=>{
+    //whenever we click on a link this will take us to the top 
+    const{pathname}=useLocation();
+    useEffect(()=>{
+        window.scrollTo(0,0);
+    },[pathname])
+    return null;
+}
 function App(){
-    return <div className='app'></div>;
+    return <div className='app'>
+    {/* Browserouter - this component utilizes the HTML5 history API to update the UI without reloading the page.  */}
+        <BrowserRouter>
+            <ScrollToTop/>
+            <Navbar/>
+            <Routes>
+                <Route  path="/" element={<Home/>}/> 
+                <Route  path="item/:itemId" element={<ItemDetails/>}/> 
+                <Route  path="checkout" element={<Checkout/>}/> 
+                <Route  path="/" element={<Home/>}/> 
+                <Route path="checkout/success" element={<Confirmation/>}/>           
+            </Routes>
+        </BrowserRouter>
+    </div>;
 }
 export default App;
