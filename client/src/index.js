d0054e582d0a4aa68261c63cb6d7150d3b03d954
Rounak sharma <sharmarounak014@gmail.com>
2023-05-31 12:03:44 +0530

    in this commit created all the state we will be requiring for managing the cart

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 import { ThemeProvider } from "@mui/material/styles";
-import {CssBaseline} from "@mui/material"
+import { CssBaseline } from "@mui/material";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
+import cartReducer from "./state";
+//This statement is importing the default export from the "./state" file
+//and assigning it to the variable cartReducer
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <CssBaseline/>
-    <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,4 +1,58 @@
+import {useDispatch,useSelector} from 'react-redux';
+import { Badge, IconButton } from "@mui/material";
+import{
+    PersonOutline,
+    ShoppingBagOutlined,
+    MenuOutlined,
+    SearchOutlined
+} from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+import {shades} from "../../theme"
+import './Nav.css';
+
 const Navbar=()=>{
-    return <div>Navbar</div>
+    const navigate=useNavigate();
+    const handleclick=()=>{
+          // Use the navigate function to navigate to a different route
+        navigate('/');
+    };
+    return <nav>
+        <div className='container'>
+        <div className='Nav_logo'onClick={handleclick}   style={{cursor: 'pointer',
+        }}>CB</div>
+        <div className='Nav_buttons'>
+        <IconButton >
+            <SearchOutlined/>
+        </IconButton>
+        <IconButton >
+            <PersonOutline/>
+        </IconButton>
+        <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              "& .MuiBadge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0 4px",
+                height: "14px",
+                minWidth: "13px",
+              },
+            }}
+          >
+            <IconButton
+              onClick={() => dispatch(setIsCartOpen({}))}
+              sx={{ color: "black" }}
+            >
+              <ShoppingBagOutlined />
+            </IconButton>
+          </Badge>
+        <IconButton style={{color:"black"}}>
+            <MenuOutlined/>
+        </IconButton>
+        </div>
+        </div>
+    </nav>
 };
 export default Navbar;
